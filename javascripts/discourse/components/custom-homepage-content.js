51996cb4ab1326dd36f89cdb1fe15608c045c3a6
pinder99 <pinder.chaudhary@gmail.com>
2024-10-18 16:23:24 +0530

    fone

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -6,7 +6,7 @@ export default class CustomHomepageContent extends Component {
 
   get isCustomHomepage() {
     const { currentURL } = this.router;
-    const customPath = '/c/start-here/62/none';
+    const customPath = '/privacy';
     console.log("Current URL:", currentURL);  // Debugging: Check the URL
     return currentURL === customPath;
   }
